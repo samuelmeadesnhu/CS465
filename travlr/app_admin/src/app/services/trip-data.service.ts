5e4ccd33148cb945eeadf54b31cec4e71741b101
samuelmeadesnhu <samuel.meade@snhu.edu>
2023-08-06 19:52:03 -0400

    module6 - step 30 front end not starting

---
+++ travlr/app_admin/src/app/services/trip-data.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+
+import { Trip } from '../models/trip'
+
+@Injectable()
+export class TripDataService {
+  
+  constructor(private http: Http) { }
+
+  private apiBaseUrl = 'http://localhost:300/api/';
+
+  public getTrips(): Promise<Trip[]> {
+    console.log('Inside TripDataServie#getTrips');
+    return this.http
+      .get(`${this.apiBaseUrl}trips`)
+      .toPromise()
+      .then(response => response.json() as Trip[])
+      .catch(this.handleError);
+  }
+
+  private handleError(error:any): Promise<any> {
+    console.error('Something has gone wrong', error); // for demo purposes only
+    return Promise.reject(error.message || error);
+  }
+}
--- travlr/app_api/routes/index.js
+++ travlr/app_api/routes/index.js
@@ -3,8 +3,16 @@ var router = express.Router();
 const tripsController = require('../controllers/trips');
 
 /* GET home page. */
-router
-    .route('/trips')
-    .get(tripsController.tripsList)
+// router
+//     .route('/trips')
+//     .get(tripsController.tripsList)
+
+// router
+//     .route('/trips/:tripCode')
+//     .get(tripsController.tripsFindCode); 
+
+router.get('/trips', tripsController.tripsList);
+router.get('/trips/:tripCode', tripsController.tripsFindCode);
+
 
 module.exports = router;
