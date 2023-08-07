dc9d1d855732c2a29684f611cabd74fd5492f9b1
samuelmeadesnhu <samuel.meade@snhu.edu>
2023-08-06 22:02:05 -0400

    module6

--- travlr/app_api/routes/index.js
+++ travlr/app_api/routes/index.js
@@ -3,16 +3,18 @@ var router = express.Router();
 const tripsController = require('../controllers/trips');
 
 /* GET home page. */
-// router
-//     .route('/trips')
-//     .get(tripsController.tripsList)
+router
+    .route('/trips')
+    .get(tripsController.tripsList)
+    .post(tripsController.tripsAddTrip);
 
-// router
-//     .route('/trips/:tripCode')
-//     .get(tripsController.tripsFindCode); 
+router
+    .route('/trips/:tripCode')
+    .get(tripsController.tripsFindCode)
+    .put(tripsController.tripsUpdateTrip);
 
-router.get('/trips', tripsController.tripsList);
-router.get('/trips/:tripCode', tripsController.tripsFindCode);
+// router.get('/trips', tripsController.tripsList);
+// router.get('/trips/:tripCode', tripsController.tripsFindCode);
 
 
 module.exports = router;
