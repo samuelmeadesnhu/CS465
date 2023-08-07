687fed6c63031f08f5db330a57320a80fb305b67
samuelmeadesnhu <samuel.meade@snhu.edu>
2023-08-06 21:43:07 -0400

    module 6 stuck loading... following the addition of edit-trip logic

--- travlr/app_admin/src/app/services/trip-data.service.ts
+++ travlr/app_admin/src/app/services/trip-data.service.ts
@@ -9,6 +9,16 @@ export class TripDataService {
   constructor(private http: Http) { }
 
   private apiBaseUrl = 'http://localhost:3000/api/';
+  private tripUrl = `${this.apiBaseUrl}trips/`;
+
+  public addTrip(formData: Trip): Promise<Trip[]> {
+    console.log('Inside TripDataServie#addTrip');
+    return this.http
+      .post(this.tripUrl, formData) // pass formData in request body
+      .toPromise()
+      .then(response => response.json() as Trip[])
+      .catch(this.handleError);
+  }
 
   public getTrips(): Promise<Trip[]> {
     console.log('Inside TripDataServie#getTrips');
@@ -22,5 +32,24 @@ export class TripDataService {
   private handleError(error:any): Promise<any> {
     console.error('Something has gone wrong', error); // for demo purposes only
     return Promise.reject(error.message || error);
+  }
+
+  public getTrip(tripCode: string): Promise<Trip> {
+    console.log('Inside TripDataService#getTrip(tripCode)');
+    return this.http
+      .get(this.tripUrl + tripCode)
+      .toPromise()
+      .then(response => response.json() as Trip)
+      .catch(this.handleError);
+  }
+
+  public updateTrip(formData: Trip): Promise<Trip> {
+    console.log('Inside TripDataService#upateTrip');
+    console.log(formData);
+    return this.http
+      .put(this.tripUrl + formData.code, formData)
+      .toPromise()
+      .then(response => response.json() as Trip[])
+      .catch(this.handleError);
   }
 }
--- travlr/app_admin/src/app/trip-card/trip-card.component.ts
+++ travlr/app_admin/src/app/trip-card/trip-card.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Router } from "@angular/router";
+import { Trip } from '../models/trip';
 
 @Component({
   selector: 'app-trip-card',
@@ -7,10 +9,19 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class TripCardComponent implements OnInit {
 
-  @Input('trip') trip: any;
-  constructor() { }
+  @Input('trip') trip: Trip;
+
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit() {
+  }
+
+  private editTrip(trip: Trip): void {
+    localStorage.removeItem("tripCode");
+    localStorage.setItem("tripCode", trip.code);
+    this.router.navigate(['edit-trip']);
   }
 
 }
